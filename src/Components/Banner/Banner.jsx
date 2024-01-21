bfbbf420ed3793b499aab577ad99681c877898d9
Ashik Hasan <120168546+Ashik-gf@users.noreply.github.com>
2024-01-21 23:59:59 +0600

    uploade this

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -22,7 +22,7 @@ export default function Banner() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 5000,
           disableOnInteraction: false,
         }}
         pagination={{
