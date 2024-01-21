46d5bbbc34ffc8a17db45bd154e304b26ab15d07
Ashik Hasan <120168546+Ashik-gf@users.noreply.github.com>
2024-01-22 00:04:43 +0600

    uplode ourEqupment

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,14 @@ import Banner from '../../Components/Banner/Banner'
 import SocialMedia from '../../Components/SocialMedia/SocialMedia'
 import OurEquep from '../../Components/OurEqp/OurEquep'
 
+
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
       <SocialMedia />
-      <OurEquep />
+    <OurEquep />
     </div>
   )
 }
