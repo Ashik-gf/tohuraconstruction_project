6d0e5ad2961a5771c4f7fad516f6cd0f8219a636
ashik-gf <bluenightskin1013@gmail.com>
2024-01-23 15:36:17 +0600

    uplode this file

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import photos from "../../assets/Image/newbanner.png"
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -9,10 +9,8 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import photos from '../../assets/Image/Banner.jpg';
-import photos2 from '../../assets/Image/banner3.png';
-import photos3 from '../../assets/Image/banner4.png';
-import photos4 from '../../assets/Image/banner5.png';
+
+
 
 export default function Banner() {
 
@@ -29,33 +27,56 @@ export default function Banner() {
           clickable: true,
         }}
         navigation={true}
-        modules={[ Autoplay, Pagination, Navigation]}
+        modules={[  Pagination, Navigation]}
         className="mySwiper"
         
       >
-        <SwiperSlide >
-        <div className=' flex justify-center items-center'>
-          <img className=' h-[90vh] md:h-[80vh] w-[900px]' src={photos} />
+        <SwiperSlide  className=' border-2 border-red-500' >
+
+        <div className=' w-12/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-[80vh] border-2'>
+
+      <div className='  w-full my-8'>
+      <div className=' flex justify-start items-center gap-8  '>
+        <div className=' h-[60px] grid grid-cols-1 gap-2 px-4'>
+        <div className="h-4 w-10 bg-orange-500"></div>
+        <div className="h-4 w-10 bg-orange-500"></div>
+        <div className="h-4 w-10 bg-orange-500"></div>
+        </div>
+        {/* TExt box */}
+        <div className=''>
+          <h1> <span className=' text-[70px] text-orange-500 font-salsa'>Tohura</span>
+          <br/>
+           <span className=' px-8 text-4xl font-bold'>Construction</span>
+           </h1>
+        </div>
+
+       </div>
+       <div className=' px-4 my-2  w-full'>
+       <h1 className=' text-wrap text-2xl font-bold font-salsa'>
+        In The Field Of Construction
+       </h1>
+       <p className=' text-justify my-4'>
+        Ingound Company is one of the most prominent real estate development companies in the industry today.
+        Founded in 1960, our exoerience in providing properties is more affordable, high quility,varied, and comfortable for you and your loved one to live in.
+       </p>
+       </div>
+       <div className=' px-4 my-4'>
+        <img src={photos}></img>
+       </div>
+      </div>
+
+
+       {/* Meddele Point Course */}
+       <div className=' h-full w-4/12 bg-blue-500 '>
+
+       </div>
+       <div className=' h-full w-4/12 bg-slate-500 '>
+
+       </div>
+      
         </div>
         </SwiperSlide>
 
-        <SwiperSlide >
-        <div className=' flex justify-center items-center'>
-          <img className=' h-[90vh] md:h-[80vh] w-[900px]' src={photos} />
-        </div>
-        </SwiperSlide>
-
-        <SwiperSlide >
-        <div className=' flex justify-center items-center'>
-          <img className=' h-[90vh] md:h-[80vh] w-[900px]' src={photos} />
-        </div>
-        </SwiperSlide>
-
-        <SwiperSlide >
-        <div className=' flex justify-center items-center'>
-          <img className=' h-[90vh] md:h-[80vh] w-[900px]' src={photos} />
-        </div>
-        </SwiperSlide>
 
       </Swiper>
     </>
